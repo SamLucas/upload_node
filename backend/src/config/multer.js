f698bc56c340f33ffed9d22825d3761f8fc3e990
Samuel Lucas Santos Gomes <samuellucas0603@gmail.com>
2020-01-27 20:21:00 -0300

    Finalizando backend.

--- backend/src/config/multer.js
+++ backend/src/config/multer.js
@@ -1,22 +1,40 @@
 const multer = require("multer");
 const path = require("path");
 const crypto = require("crypto");
+const multerS3 = require("multer-s3");
+const awsSdk = require("aws-sdk");
 
-module.exports = {
-  dest: path.resolve(__dirname, "..", "..", "tmp", "uploads"),
-  storage: multer.diskStorage({
+const storangeTypes = {
+  local: multer.diskStorage({
     destination: (req, file, cb) => {
       cb(null, path.resolve(__dirname, "..", "..", "tmp", "uploads"));
     },
     filename: (req, file, cb) => {
       crypto.randomBytes(16, (err, hash) => {
         if (err) cb(err);
-
+        file.key = `${hash.toString("hex")}-${file.originalname}`;
+        cb(null, file.key);
+      });
+    }
+  }),
+  s3: multerS3({
+    s3: new awsSdk.S3(),
+    bucket: process.env.BUCKET,
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    acl: "public-read",
+    key: (req, file, cb) => {
+      crypto.randomBytes(16, (err, hash) => {
+        if (err) cb(err);
         const fileName = `${hash.toString("hex")}-${file.originalname}`;
         cb(null, fileName);
       });
     }
-  }),
+  })
+};
+
+module.exports = {
+  dest: path.resolve(__dirname, "..", "..", "tmp", "uploads"),
+  storage: storangeTypes[process.env.STORANGE_TYPE],
   limits: {
     fileSize: 2 * 1024 * 1024
   },
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,9 +2,31 @@ const routes = require("express").Router();
 const multer = require("multer");
 const multerConfig = require("./config/multer");
 
-routes.post("/posts", multer(multerConfig).single("file"), (req, res) => {
-  console.log(req.file);
-  return res.json({ hello: "Ola" });
+const Post = require("./models/Post");
+
+routes.post("/posts", multer(multerConfig).single("file"), async (req, res) => {
+  let { originalname: name, size, key, location: url = "" } = req.file;
+
+  const post = await Post.create({
+    name,
+    size,
+    key,
+    url
+  });
+
+  return res.json(post);
+});
+
+routes.get("/posts", async (req, res) => {
+  const posts = await Post.find();
+  res.send(posts);
+});
+
+routes.delete("/posts/:id", async (req, res) => {
+  const posts = await Post.findById(req.params.id);
+  await posts.remove();
+
+  res.send();
 });
 
 module.exports = routes;
