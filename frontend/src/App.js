8f8ad3de934e1e92b4f5f4d0289559ed91baddd4
Samuel Lucas Santos Gomes <samuellucas0603@gmail.com>
2020-01-27 23:21:38 -0300

    Criando componente do upload.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,15 @@ import React from "react";
 import GlobalStyle from "./styles/global";
 import { Container, Content } from "./styles";
 
+import Upload from "./components/Upload";
+
 export default function src() {
   return (
     <Container>
-      <Content>teste</Content>
+      <Content>
+        <Upload />
+      </Content>
+
       <GlobalStyle />
     </Container>
   );
--- frontend/src/components/Upload/index.js
+++ frontend/src/components/Upload/index.js
@@ -1,7 +1,32 @@
 import React from "react";
 
-// import { Container } from './styles';
+import Dropzone from "react-dropzone";
+import { DropContainer, UploadMensage } from "./styles";
 
 export default function Upload() {
-  return <div />;
+  const renderDragMensage = (isDragActive, isDragReject) => {
+    if (!isDragActive) {
+      return <UploadMensage>Arraste arquivo aqui...</UploadMensage>;
+    }
+    if (isDragReject) {
+      return <UploadMensage type="error">Arraste não suportado</UploadMensage>;
+    }
+
+    return <UploadMensage type="success">Solte os arquivos </UploadMensage>;
+  };
+
+  return (
+    <Dropzone accept="image/*" onDropAccepted={() => {}}>
+      {({ getRootProps, getInputProps, isDragActive, isDragReject }) => (
+        <DropContainer
+          {...getRootProps()}
+          isDragActive={isDragActive}
+          isDragReject={isDragReject}
+        >
+          <input {...getInputProps()} />
+          {renderDragMensage(isDragActive, isDragReject)}
+        </DropContainer>
+      )}
+    </Dropzone>
+  );
 }
