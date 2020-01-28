b9bf3a9c6043d204a4052de784983e427f9fbac8
Samuel Lucas Santos Gomes <samuellucas0603@gmail.com>
2020-01-27 21:14:41 -0300

    Finalizando parte global da aplicação

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import GlobalStyle from "./styles/global";
+import { Container, Content } from "./styles";
+
+export default function src() {
+  return (
+    <Container>
+      <Content>teste</Content>
+      <GlobalStyle />
+    </Container>
+  );
+}
